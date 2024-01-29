61f4b72564b36416d257c3b8bb0417a5fa99fd87
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2024-01-29 15:15:53 +0100

    Changes

--- src/main/java/com/example/pharmdemo/enums/ResponseCode.java
+++ src/main/java/com/example/pharmdemo/enums/ResponseCode.java
@@ -12,7 +12,9 @@ public enum ResponseCode {
     DRUG_UPDATED_SUCCESSFULLY(406,"Updated drug successfully"),
     DRUG_DELETED_SUCCESSFULLY(407,"Deleted drug successfully"),
     GOT_DRUG_SUCCESSFULLY(408,"Got drug successfully"),
-    GOT_ALL_DRUG_SUCCESSFULLY(409,"Got all drugs successfully");
+    GOT_ALL_DRUG_SUCCESSFULLY(409,"Got all drugs successfully"),
+    USER_CREATED_SUCCESSFULLY(500,"Created user successfully");
+
 
     private int code;
     private String message;
---
+++ src/main/java/com/example/pharmdemo/enums/Role.java
@@ -0,0 +1,5 @@
+package com.example.pharmdemo.enums;
+
+public enum Role {
+    ROLE_USER, ROLE_PHARMACIST, ROLE_ADMIN
+}
