c4d445d5e5df7ad8eeba540d16046f09ebc77044
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2024-01-31 10:55:11 +0100

    Changes

---
+++ src/main/java/com/example/pharmdemo/dto/request/AuthenticationRequest.java
@@ -0,0 +1,18 @@
+package com.example.pharmdemo.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+   private  String username;
+
+   String password;
+
+
+}
