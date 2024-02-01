e53a09d9e53f33423d2655c2ae9279b584df68c7
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2024-02-01 10:45:04 +0100

    Changes

--- src/main/java/com/example/pharmdemo/service/UserService.java
+++ src/main/java/com/example/pharmdemo/service/UserService.java
@@ -3,8 +3,10 @@ package com.example.pharmdemo.service;
 import com.example.pharmdemo.dto.request.UserRequest;
 import com.example.pharmdemo.dto.respone.ApiResponse;
 import com.example.pharmdemo.dto.respone.UserResponse;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 public interface UserService {
     ApiResponse<UserResponse> signUp(@RequestBody UserRequest userRequest);
+    ApiResponse<UserResponse> login(@PathVariable String userName);
 }
