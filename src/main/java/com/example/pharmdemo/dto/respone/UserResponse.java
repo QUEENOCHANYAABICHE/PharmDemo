d23a6ecc233782f0f14377edbfebd71c4acf3d8e
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2024-02-06 13:03:23 +0100

    Changes

--- src/main/java/com/example/pharmdemo/dto/respone/UserResponse.java
+++ src/main/java/com/example/pharmdemo/dto/respone/UserResponse.java
@@ -1,5 +1,6 @@
 package com.example.pharmdemo.dto.respone;
 
+import com.example.pharmdemo.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,4 +11,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponse {
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private Boolean status;
+
+    private int age;
+
+    private Role role;
+
+    private String phoneNumber;
+
+    private String password;
 }
