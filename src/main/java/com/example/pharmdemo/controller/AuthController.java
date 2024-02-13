7ac43d797e1035c50ca469a7816aed9d4c458617
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2024-02-13 23:45:34 +0100

    Changes

--- src/main/java/com/example/pharmdemo/controller/AuthController.java
+++ src/main/java/com/example/pharmdemo/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.example.pharmdemo.controller;
 
+import com.example.pharmdemo.dto.request.LoginRequest;
+import com.example.pharmdemo.dto.request.LogoutRequest;
+import com.example.pharmdemo.dto.request.RegistrationRequest;
 import com.example.pharmdemo.dto.request.UserRequest;
-import com.example.pharmdemo.dto.respone.ApiResponse;
-import com.example.pharmdemo.dto.respone.UserResponse;
-import com.example.pharmdemo.service.serviceImpl.UserServiceImpl;
+import com.example.pharmdemo.dto.respone.*;
+import com.example.pharmdemo.service.serviceImpl.AuthenticationServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,14 +18,34 @@ import static com.example.pharmdemo.utils.AuthEndpoints.*;
 @RequestMapping(AUTH_CONTROLLER)
 public class AuthController {
 
-    private final UserServiceImpl userService;
-    @PostMapping(SIGN_UP)
-    public ResponseEntity<ApiResponse<UserResponse>> signUp(@RequestBody UserRequest userRequest){
-        return new ResponseEntity<>(userService.signUp(userRequest), HttpStatus.CREATED);
+
+    private final AuthenticationServiceImpl authenticationService;
+//    @PostMapping(SIGN_UP)
+//    public ResponseEntity<ApiResponse<UserResponse>> signUp(@RequestBody UserRequest userRequest){
+//        return new ResponseEntity<>(authenticationService.register(userRequest), HttpStatus.CREATED);
+//    }
+//
+//    @PostMapping(LOGIN)
+//    public ResponseEntity<ApiResponse<UserResponse>> login(@PathVariable String username){
+//        return new ResponseEntity<>(authenticationService.login(username), HttpStatus.OK);
+//    }
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse<AuthenticationResponse>> register(@RequestBody RegistrationRequest request){
+        return new ResponseEntity<>(authenticationService.register(request), HttpStatus.CREATED);
     }
 
-    @PostMapping(LOGIN)
-    public ResponseEntity<ApiResponse<UserResponse>> login(@PathVariable String userName){
-        return new ResponseEntity<>(userService.login(userName), HttpStatus.OK);
+    @PostMapping("/authenticate")
+    public ResponseEntity<ApiResponse<LoginResponse>> authenticate(@RequestBody LoginRequest request){
+        return new ResponseEntity<>(authenticationService.authenticate(request), HttpStatus.OK);
+
     }
+
+    @PostMapping(LOGOUT)
+    public ResponseEntity<ApiResponse<LogoutResponse>> logout(@RequestBody LogoutRequest logoutRequest){
+        return new ResponseEntity<>(authenticationService.logout(logoutRequest), HttpStatus.OK);
+    }
+
+
+
 }
