36057fc4df5fb11482e8c522dbe748775fc97b8f
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2024-02-16 13:32:23 +0100

    Changes

--- src/main/java/com/example/pharmdemo/applicationConfig/security/JwtService.java
+++ src/main/java/com/example/pharmdemo/applicationConfig/security/JwtService.java
@@ -25,97 +25,83 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Slf4j
 public class JwtService {
-
     @Value("${jwt.expiration.access_token}")
     private long accessTokenExpiration;
-
     @Value("${jwt.expiration.refresh_token}")
     private long refreshTokenExpiration;
 
     @Value("${jwt.secret_key}")
     private String generateSecret;
 
-    //private static final String SECRET_KEY ="b1sDLs2KnB3k5i82jBAyZe7VfV1SQZ5lhdudheueygeryrgfgfyrfuere";
-
-//    public Key getSignInKey() {
-//        byte[] signedInKey = Decoders.BASE64.decode(generateSecret);
-//        return Keys.hmacShaKeyFor(signedInKey);
-//    }
-
-    public Key getSignInKey() {
-        String base64EncodedKey = "QWZmZmhoZWZhc2RmZ2hqa3BvaXV5dHJld3Fhc2RmZ2hqa2xrbW5idmN4emFzZGZnaGprb2l1eXRyZWR3c3Fhc2RmZ2hqa2xramhndg==";
-        byte[] signedInKey = Decoders.BASE64.decode(base64EncodedKey);
-        return Keys.hmacShaKeyFor(signedInKey);
+    public Key getSignInKey(){
+        byte [] signedInkey  = Decoders.BASE64.decode(generateSecret);
+        return Keys.hmacShaKeyFor(signedInkey);
     }
 
-
-    public Claims extractAllClaims(String token) {
-        return Jwts.parserBuilder()
+    public Claims extractAllClaim(String token){
+        return Jwts
+                .parserBuilder()
                 .setSigningKey(getSignInKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
 
-    public String extractUserName(String token) {
-        Claims claims = extractAllClaims(token);
+    public String extractUsername(String token){
+        Claims claims = extractAllClaim(token);
         return claims.getSubject();
     }
 
-    public <T> T extractSingleClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
+    public <T> T extractSingleClaim(String token, Function<Claims, T>  claimResolver){
+        Claims claims = extractAllClaim(token);
+        return claimResolver.apply(claims);
     }
-
-
-    public Date extractExpiration(String token) {
+    public Date extractExpiration(String token){
         return extractSingleClaim(token, Claims::getExpiration);
     }
-
-    public boolean isTokenExpired(String token) {
+    public boolean isTokenExpired(String token){
         return extractExpiration(token).before(new Date());
     }
 
-    public String buildToken(UserDetails userDetails, Map<String, Object> extractClaims, long expiration) {
+    public String buildToken(
+            UserDetails userDetails,
+            Map<String, Object> extractClaims,
+            long expiration){
         return Jwts
                 .builder()
                 .setClaims(extractClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + expiration))
-                .signWith(getSignInKey(), SignatureAlgorithm.RS256)
+                .signWith(getSignInKey(), SignatureAlgorithm.HS512)
                 .compact();
     }
 
     public String getExtractedClaims(
             UserDetails userDetails,
             Map<String, Object> extractClaim,
-            long expiration) {
+            long expiration){
         Set<String> role = userDetails.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toSet());
         extractClaim.put("role", role);
-        return buildToken(userDetails, extractClaim, expiration);
+        return buildToken(userDetails,extractClaim,expiration);
     }
-
-    public String generateToken(UserDetails userDetails) {
-        return getExtractedClaims(userDetails, new HashMap<>(), accessTokenExpiration);
+    public String generateToken(UserDetails userDetails){
+        return getExtractedClaims(userDetails,new HashMap<>(),accessTokenExpiration);
 
     }
-
     public String generateRefreshToken(
             UserDetails userDetails
-    ) {
-        return buildToken(userDetails, new HashMap<>(), refreshTokenExpiration);
+    ){
+        return buildToken(userDetails,new HashMap<>(),refreshTokenExpiration);
     }
 
-    public boolean isTokenValid(String token, UserDetails userDetails) {
-        final String username = extractUserName(token);
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
 
     }
-
 }
 
 
