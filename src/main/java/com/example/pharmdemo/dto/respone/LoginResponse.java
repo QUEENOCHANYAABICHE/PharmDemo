4cdb6e5d1a48a6f17663d270d013f60ab090afc7
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2024-02-16 13:33:11 +0100

    Changes

---
+++ src/main/java/com/example/pharmdemo/dto/respone/LoginResponse.java
@@ -0,0 +1,16 @@
+package com.example.pharmdemo.dto.respone;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String username;
+    private String accessToken;
+    private String refreshToken;
+}
