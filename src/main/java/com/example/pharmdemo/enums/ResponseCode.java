aa1796a6ba1c4a76eeb792fd1442d5c142d31a08
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2024-02-19 13:27:56 +0100

    Changes

--- src/main/java/com/example/pharmdemo/enums/ResponseCode.java
+++ src/main/java/com/example/pharmdemo/enums/ResponseCode.java
@@ -13,8 +13,11 @@ public enum ResponseCode {
     DRUG_DELETED_SUCCESSFULLY(407,"Deleted drug successfully"),
     GOT_DRUG_SUCCESSFULLY(408,"Got drug successfully"),
     GOT_ALL_DRUG_SUCCESSFULLY(409,"Got all drugs successfully"),
-    USER_CREATED_SUCCESSFULLY(500,"Created user successfully");
-
+    USER_CREATED_SUCCESSFULLY(500,"Created user successfully"),
+    VERIFICATION_SUCCESS_RESPONSE(501, "User verified successfully"),
+    USER_REGISTERED_SUCCESSFULLY(502, "User registered successfully"),
+    LOGIN_SUCCESSFULLY_RESPONSE(503, "User logged in successfully"),
+    LOGOUT_SUCCESSFULLY_RESPONSE(504, "User logged out successfully");
 
     private int code;
     private String message;
