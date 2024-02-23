b8f5fd9b1afd8c6c0765593d219ff5cd268f8a6f
QUEENOCHANYAABICHE <abichequeen@gmail.com>
2024-02-23 16:26:08 +0100

    Changes

--- src/main/java/com/example/pharmdemo/applicationConfig/ApplicationConfig.java
+++ src/main/java/com/example/pharmdemo/applicationConfig/ApplicationConfig.java
@@ -1,18 +1,71 @@
 package com.example.pharmdemo.applicationConfig;
 
+import com.example.pharmdemo.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
+    private final UserRepository repository;
 
     @Bean
-    UserDetailsService userDetailsService(){
-        return new
+    public ObjectMapper objectMapper(){
+      return new ObjectMapper();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+     return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public HttpHeaders httpHeaders(){
+        return new HttpHeaders();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+
+
+
+
+
 
 }
